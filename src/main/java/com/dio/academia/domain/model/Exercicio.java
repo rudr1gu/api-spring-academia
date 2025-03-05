f71f633df780bbdb759d2d33d537f5a942e58653
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-03-05 13:47:22 -0300

    :sparkles: add Exercicio entity with fields and relationship to Treino

--- src/main/java/com/dio/academia/domain/model/Exercicio.java
+++ src/main/java/com/dio/academia/domain/model/Exercicio.java
@@ -1,5 +1,31 @@
 package com.dio.academia.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "exercicios")
 public class Exercicio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String grupoMuscular;
+    private int series;
+    private int repeticoes;
+
+    @ManyToOne
+    @JoinColumn(name = "treino_id", nullable = false)
+    private Treino treino;
 }
