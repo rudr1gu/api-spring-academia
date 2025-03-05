699bd77afa7a678958c99ad81fb87487beda2eb3
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-03-05 13:50:28 -0300

    :sparkles: add TreinoRepository interface for Treino entity persistence

--- src/main/java/com/dio/academia/domain/repository/TreinoRepository.java
+++ src/main/java/com/dio/academia/domain/repository/TreinoRepository.java
@@ -3,9 +3,9 @@ package com.dio.academia.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dio.academia.domain.model.Aluno;
+import com.dio.academia.domain.model.Treino;
 
 @Repository
-public interface AlunoRepository  extends JpaRepository<Aluno, Long> {
+public interface TreinoRepository extends JpaRepository<Treino, Long> {
     
 }
