57648a6153410c0d5e69f6432033c15d5a3c02fd
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-03-05 13:45:57 -0300

    :sparkles: add Exercicio entity and update Treino entity with new fields and relationships

---
+++ src/main/java/com/dio/academia/domain/model/Exercicio.java
@@ -0,0 +1,5 @@
+package com.dio.academia.domain.model;
+
+public class Exercicio {
+
+}
--- src/main/java/com/dio/academia/domain/model/Treino.java
+++ src/main/java/com/dio/academia/domain/model/Treino.java
@@ -1,13 +1,17 @@
 package com.dio.academia.domain.model;
 
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,18 +19,22 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "tb_treino")
+@Table(name = "treinos")
 public class Treino {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String nome;
-
     private String objetivo;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
 
-    private Date dataInicio;
+    @ManyToOne
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
 
-    private Date dataFim;
+    @OneToMany(mappedBy = "treino", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Exercicio> exercicios;
 }
