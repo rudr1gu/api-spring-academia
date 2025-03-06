963a0765120c8d3bc102056242cd897410dff1e2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-03-06 11:54:59 -0300

    :books: doc add Springdoc OpenAPI dependency and configuration for API documentation

--- src/main/java/com/dio/academia/controller/AlunoController.java
+++ src/main/java/com/dio/academia/controller/AlunoController.java
@@ -17,9 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
